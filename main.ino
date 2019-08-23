b2e7dcdf0b4305f46069b595fc23e223edffc070
Aman Malhotra <33927579+thisisamanmalhotra@users.noreply.github.com>
2019-08-23 17:38:06 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,12 +2,7 @@
 #include <stdio.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
-#include <Wire.h> 
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
-#define OLED_ADDR1   0x3C
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 32 
+
 
 Adafruit_SSD1306 display1(-1);
 #define WIFI_SSID "pk"
@@ -21,10 +16,6 @@ Adafruit_SSD1306 display1(-1);
 int led = D7;
 
 WiFiClient client;
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERV, MQTT_PORT, MQTT_NAME, MQTT_PASS);
-
-Adafruit_MQTT_Subscribe message = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/f/message");
-Adafruit_MQTT_Publish LightsStatus = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/LightsStatus");
 
 
 void setup()
@@ -32,23 +23,7 @@ void setup()
   Serial.begin(9600);
   display1.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR1);
   display1.clearDisplay();
-  display1.setTextSize(2);
-    display1.setTextColor(WHITE);
-    display1.setCursor(30,0);
-    display1.println("NANO");
-    display1.display();
-     display1.setTextSize(1);
-    display1.setTextColor(WHITE);
-    display1.setCursor(20,20);
-    display1.println("Messenger");
-    display1.display();
-    delay(5000);
-   display1.clearDisplay();
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.print("\n\nConnecting Wifi>");
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  digitalWrite(LED_BUILTIN, LOW);
-
+  
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(">");
@@ -108,29 +83,4 @@ void MQTT_connect()
 {
 
   
-  if (mqtt.connected() && mqtt.ping())
-  {
-    
-    return;
-  }
-
-  int8_t ret;
-
-  mqtt.disconnect();
-
-  Serial.print("Connecting to MQTT... ");
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) 
-  {
-    Serial.println(mqtt.connectErrorString(ret));
-    Serial.println("Retrying MQTT connection in 5 seconds...");
-    mqtt.disconnect();
-    delay(5000);  
-    retries--;
-    if (retries == 0)
-    {
-      ESP.reset();
-    }
-  }
-  Serial.println("MQTT Connected!");
 }
